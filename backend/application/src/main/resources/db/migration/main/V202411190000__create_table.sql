7a1d8b83ce070ab21cea3dddad2c250d89eb31ec
Ramesh Kumhar <ramesh.kumhar@gmail.com>
2024-11-27 16:21:04 +0530

    Alter query for schema column added,

--- backend/application/src/main/resources/db/migration/main/V202411190000__create_table.sql
+++ backend/application/src/main/resources/db/migration/main/V202411190000__create_table.sql
@@ -1,4 +1,7 @@
 ALTER TABLE eg_ubp_application
+ADD COLUMN schema JSONB NOT NULL DEFAULT '{}'::JSONB;
+
+ALTER TABLE eg_ubp_application
 ADD COLUMN order_id character varying(64);
 
 ALTER TABLE eg_ubp_application
