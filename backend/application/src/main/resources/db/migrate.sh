6f2de90f6203e9275898adb85143591eee52282c
Ramesh Kumhar <ramesh.kumhar@gmail.com>
2024-12-02 17:14:59 +0530

    changes in sh file

--- backend/application/src/main/resources/db/migrate.sh
+++ backend/application/src/main/resources/db/migrate.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true   -outOfOrder=true migrate
+# Repair Flyway schema history to fix any inconsistencies
+flyway repair -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true
+
+# Apply the migration to the database
+flyway migrate -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true
