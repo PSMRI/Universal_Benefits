63aaf57cc162ea9ef1781475b23f4c7e08133709
priyankakiW <priyanka.k@iworktech.com>
2024-12-03 16:53:37 +0530

    Controller

--- backend/application/src/main/java/digit/application/web/controllers/V1ApiController.java
+++ backend/application/src/main/java/digit/application/web/controllers/V1ApiController.java
@@ -186,9 +186,11 @@ public class V1ApiController {
  
 				ApplicationUpdateBatchIDResponse BatchIDresponse = null;
 				if(batchId != null) {
+					System.out.println("Inside if batchId != null");
 					ApplicationUpdateBatchIDRequest applicationRequest = new ApplicationUpdateBatchIDRequest();
 					applicationRequest.setApplicationId(body.getApplicationId());
 					applicationRequest.setBatch_id(batchId); // You can set the status as per the incoming body
+					System.out.println("batch id inside if "+batchId);
 					 BatchIDresponse = applicationService.updateApplication_BatchId(applicationRequest);
 				}
 				// update Status
